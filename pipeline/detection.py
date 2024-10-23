914bf6d6e7170ef283f54c60b01cc16e377ea973
JoanneRCSheppard <joannesheppard1997@outlook.com>
2024-10-23 09:48:17 -0400

    time chunking

--- pipeline/detection.py
+++ pipeline/detection.py
@@ -12,51 +12,28 @@ import glob
 import numpy as np
 
 
-def process_traces(recording_path, p):
+def process_traces(recording_path, probe):
     recording = se.read_binary(recording_path, sampling_frequency=cfg.SAMPLE_RATE, dtype='float32', num_channels=cfg.N_CHANNELS_SHANK)
-    recording = recording.set_probe(p)
+    recording = recording.set_probe(probe)
     return recording
 
+
 def replace_params(file, output_path, n_channels):
-    params_file = f"""n_channels_dat = {int(n_channels)}
-offset = 0
-sample_rate = 30000
-dtype = 'float32'
-hp_filtered = False
-dat_path = '{file}'"""
-    file = str(output_path) + '/params.py'
-    with open(file, 'w') as filetowrite:
-        filetowrite.write(params_file)
+    params_content = (
+        f"n_channels_dat = {int(n_channels)}\n"
+        "offset = 0\n"
+        "sample_rate = 30000\n"
+        "dtype = 'float32'\n"
+        "hp_filtered = False\n"
+        f"dat_path = '{file}'"
+    )
+    output_file = f"{output_path}/params.py"
+    
+    with open(output_file, 'w') as f:
+        f.write(params_content)
 
 
-if __name__ == "__main__":
-
-    # Read command line arguments
-    user_input = Path(sys.argv[1])
-    dredge = sys.argv[2].lower() == 'true'
-    probe = sys.argv[3]
-    shank = sys.argv[4]
-    data_path = sys.argv[5]
-
-    data_folder = user_input / data_path
-    data_folder = data_folder / "output" / f"probe_{probe}" / f"shank_{shank}.0"
-    if dredge:
-        data_folder = data_folder / 'dredge' 
-    recording_path = str(data_folder) + '/recording/traces_cached_seg0.raw'
-    ks_path = data_folder / 'kilosort4'
-    src_dir = ks_path / 'sorter_output'
-
-    # use gpu
-    use_cuda = torch.cuda.is_available()
-    device = "cuda" if use_cuda else "cpu"
-    print("Device: ", device)
-    kilosort_params = cfg.KILOSORT_PARAMS
-    kilosort_params['torch_device'] = device
-
-    p, probe_data = ut.load_probe_from_json(cfg.SHANK_FILE)
-
-    recording = process_traces(recording_path, p=p)
-        
+def run_kilosort(recording, ks_path, src_dir, kilosort_params):
     sorting_KS4 = si.run_sorter(
         sorter_name="kilosort4",
         recording=recording,
@@ -68,7 +45,7 @@ if __name__ == "__main__":
         **kilosort_params
     )
 
-    phy_sorting = si.read_phy(folder_path= str(ks_path) + '/sorter_output')
+    phy_sorting = si.read_phy(folder_path=str(ks_path) + '/sorter_output')
     phy_sorting = si.remove_excess_spikes(sorting=sorting_KS4, recording=recording)
 
     we = si.create_sorting_analyzer(recording=recording, sorting=phy_sorting, folder=ks_path, overwrite=True, **cfg.JOB_KWARGS)
@@ -95,5 +72,59 @@ if __name__ == "__main__":
         if os.path.isfile(file_path) and not os.path.isfile(f'{str(phy_folder)}/{filename}'):
             copyfile(file_path, f'{str(phy_folder)}/{filename}')
 
-    print("Kilosort output: ", str(ks_path) + '/sorter_output/')
-    print("Phy output: ", str(phy_folder))
+    print(f"Kilosort output: {ks_path}/sorter_output/")
+    print(f"Phy output: {phy_folder}")
+
+
+if __name__ == "__main__":
+
+    # Read command line arguments
+    user_input = Path(sys.argv[1])
+    dredge = sys.argv[2].lower() == 'true'
+    probe = sys.argv[3]
+    shank = sys.argv[4]
+    data_path = sys.argv[5]
+    chunk = sys.argv[6]
+
+    data_folder = user_input / data_path / "output" / f"probe_{probe}" / f"shank_{shank}.0"
+    if dredge:
+        data_folder = data_folder / 'dredge' 
+
+    recording_path = str(data_folder) + '/recording/traces_cached_seg0.raw'
+    ks_path = data_folder / 'kilosort4'
+    src_dir = ks_path / 'sorter_output'
+
+    # Use GPU if available
+    use_cuda = torch.cuda.is_available()
+    device = "cuda" if use_cuda else "cpu"
+    print(f"Device: {device}")
+    kilosort_params = cfg.KILOSORT_PARAMS
+    kilosort_params['torch_device'] = device
+
+    # Load probe information
+    probe, probe_data = ut.load_probe_from_json(cfg.SHANK_FILE)
+
+    # Process traces
+    recording = process_traces(recording_path, probe=probe)
+        
+    if chunk is None:
+        run_kilosort(recording, ks_path, src_dir, kilosort_params)
+    
+    else:
+        chunk = int(chunk)
+        time_chunk = chunk * 60
+        chunk_counter = 1
+
+        for i in range(0, recording.get_num_frames(), time_chunk):
+            sub_recording = recording.time_slice(i, i + time_chunk)
+            
+            # Create subfolder for chunked output
+            chunk_folder = ks_path / f'chunk_{chunk_counter}'
+            chunk_folder.mkdir(parents=True, exist_ok=True)
+
+            run_kilosort(sub_recording, chunk_folder, src_dir, kilosort_params)
+
+            print(f"Processed chunk {chunk_counter} in folder {chunk_folder}")
+            chunk_counter += 1
+
+            del sub_recording
