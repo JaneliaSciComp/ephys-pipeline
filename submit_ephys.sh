da24a33579954a757e05296c00fef48abd744129
joannercsheppard <joannesheppard1997@outlook.com>
2026-03-03 12:15:42 -0500

    fix conda activate

--- submit_ephys.sh
+++ submit_ephys.sh
@@ -20,7 +20,7 @@ fi
 DIR_NAME="$(basename "$DAY_DIR")"
 BASE_DIR="/groups/voigts/voigtslab/submit_a_day"
 SCRIPT_DIR="$BASE_DIR/ephys-pipeline"
-ENV_BIN="$SCRIPT_DIR/envs/spikenv411/bin"
+ENV_BIN="$SCRIPT_DIR/envs/spikenv411/"
 
 user="${USER:-$(whoami)}"
 email="${user}@janelia.hhmi.org"
@@ -42,7 +42,7 @@ for probe in a b; do
          -N -u "$email" \
          -oo "$DAY_DIR/output/${JOB_NAME}.%J.out" \
          -eo "$DAY_DIR/output/${JOB_NAME}.%J.err" \
-         bash -lc "'$ENV_BIN/python' -u '$SCRIPT_DIR/run_pipeline.py' '$DAY_DIR' '$probe' '$shank_num'"
+         bash -lc " conda activate '$ENV_BIN' -u '$SCRIPT_DIR/run_pipeline.py' '$DAY_DIR' '$probe' '$shank_num'"
   done
 done
 
